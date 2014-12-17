03640706054ab694401fb7eb406ee12cbf612a54
c.eschia <c.eschia@ternium.techint.net>
2014-12-17 09:30:26 -0300

    Primera prueba de tomar un objeto insertado en el Modelo e imprimirlo en pantalla

--- src/main/java/com/proeza/sgs/controller/AdminController.java
+++ src/main/java/com/proeza/sgs/controller/AdminController.java
@@ -1,15 +1,29 @@
 package com.proeza.sgs.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.proeza.security.dao.UsuarioDao;
+import com.proeza.security.entity.Usuario;
+
 @Controller
 public class AdminController {
 
+	@Autowired
+	private UsuarioDao	usuarioDao;
+
 	@RequestMapping({"/admin"})
-	public ModelAndView home (ModelAndView model) {
+	public ModelAndView home (ModelAndView model, Principal principal) {
+		User activeUser = (User) ((Authentication) principal).getPrincipal();
+		Usuario usuario = this.usuarioDao.findByAlias(activeUser.getUsername());
+		model.addObject("usuario", usuario);
 		model.setViewName("admin");
 		return model;
 	}
