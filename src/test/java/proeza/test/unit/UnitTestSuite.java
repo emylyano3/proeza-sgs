31f9a598f3103838e11ab3aee77f89bb412bf9d4
emylyano3 <emylyano3@gmail.com>
2015-06-01 23:24:36 -0300

    - Nuevos tests agregados al suite general

--- src/test/java/proeza/test/unit/UnitTestSuite.java
+++ src/test/java/proeza/test/unit/UnitTestSuite.java
@@ -6,12 +6,14 @@ import org.junit.runners.Suite;
 import proeza.test.unit.settings.SettingsTest;
 import proeza.test.unit.viewmenu.ViewMenuManagerTest;
 import proeza.test.unit.web.controller.ControllerTestSuite;
+import proeza.test.unit.web.rest.RestControllerTestSuite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	ControllerTestSuite.class,
-	ViewMenuManagerTest.class,
-	SettingsTest.class
+    ControllerTestSuite.class,
+    RestControllerTestSuite.class,
+    ViewMenuManagerTest.class,
+    SettingsTest.class
 })
 public class UnitTestSuite {
 }
---
+++ src/test/java/proeza/test/unit/web/rest/RestControllerTestSuite.java
@@ -0,0 +1,11 @@
+package proeza.test.unit.web.rest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+    ArticuloRestControllerTest.class
+})
+public class RestControllerTestSuite {
+}
