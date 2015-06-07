e5f1d11d7e2f0409f8ab1991da5f441e6f2e95c5
emylyano3 <emylyano3@gmail.com>
2015-06-07 13:34:21 -0300

    + Agurego el rest controller de usuario

---
+++ src/main/java/com/proeza/sgs/web/rest/UsuariosRestController.java
@@ -0,0 +1,39 @@
+package com.proeza.sgs.web.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proeza.security.service.IUserService;
+import com.proeza.sgs.business.dto.UsuarioDTO;
+
+@RestController
+@RequestMapping("rest/usuario")
+public class UsuariosRestController {
+	
+	@Autowired
+	private IUserService	userService;
+
+	@RequestMapping(value = "findAll")
+	public List<UsuarioDTO> list () {
+		return this.userService.findAll();
+	}
+
+//	@RequestMapping(value = "/rest/usuarios/search", method = RequestMethod.GET)
+//	public List<UsuarioDTO> search (@RequestParam("filter") String filter) {
+//		return this.userService.findByStringFilter(filter);
+//	}
+	
+	@RequestMapping(value = "update", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update (@RequestBody UsuarioDTO usuario) {
+		this.userService.update(usuario);
+	}
+
+}
