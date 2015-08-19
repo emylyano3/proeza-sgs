d7af81d2bf9bbee09cca6a6d81113e417dc0bf5c
emylyano3 <emylyano3@gmail.com>
2015-08-19 11:04:09 -0300

    + Nueva clase de constantes en el core

---
+++ src/main/java/com/proeza/core/util/Constants.java
@@ -0,0 +1,7 @@
+package com.proeza.core.util;
+
+public class Constants {
+    public static final int BYTE = 1;
+    public static final int KB   = 1024;
+    public static final int MB   = 1024 * 1024;
+}
