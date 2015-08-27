7426e4d7e1fab3daded05eed9621e4bc26f4e990
emylyano3 <emylyano3@gmail.com>
2015-08-27 20:44:48 -0300

    ! find rol por codigo

--- src/main/java/com/proeza/security/dao/IRolDao.java
+++ src/main/java/com/proeza/security/dao/IRolDao.java
@@ -3,4 +3,6 @@ package com.proeza.security.dao;
 import com.proeza.core.persistence.Dao;
 import com.proeza.security.entity.Rol;
 
-public interface IRolDao extends Dao<Rol> {}
+public interface IRolDao extends Dao<Rol> {
+    Rol findByCode (String code);
+}
--- src/main/java/com/proeza/security/dao/impl/RolDao.java
+++ src/main/java/com/proeza/security/dao/impl/RolDao.java
@@ -1,10 +1,25 @@
 package com.proeza.security.dao.impl;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.springframework.stereotype.Repository;
 
 import com.proeza.core.persistence.BaseDao;
 import com.proeza.security.dao.IRolDao;
 import com.proeza.security.entity.Rol;
+import com.proeza.security.entity.Rol_;
 
 @Repository
-public class RolDao extends BaseDao<Rol> implements IRolDao {}
+public class RolDao extends BaseDao<Rol> implements IRolDao {
+
+    public Rol findByCode (String code) {
+        CriteriaBuilder builder = this.entityManager.getCriteriaBuilder();
+        CriteriaQuery<Rol> criteria = builder.createQuery(Rol.class);
+        Root<Rol> root = criteria.from(Rol.class);
+        criteria.select(root);
+        criteria.where(builder.equal(root.get(Rol_.codigo), code));
+        return this.entityManager.createQuery(criteria).getSingleResult();
+    }
+}
