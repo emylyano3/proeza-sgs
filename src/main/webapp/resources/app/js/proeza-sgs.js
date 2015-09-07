fa526b84c2f5af67b8f20e9c71899bcea5a26f53
emylyano3 <emylyano3@gmail.com>
2015-09-07 10:19:07 -0300

    ! Mejoras de interfaz ! Renombre de constantes de texto ! Refactor y ordenamiento del javascript de usuario y producto

--- src/main/webapp/resources/app/js/proeza-sgs.js
+++ src/main/webapp/resources/app/js/proeza-sgs.js
@@ -100,26 +100,26 @@ function replaceAll(text, busca, reemplaza ){
 }
 
 function fixWrapperHeight() {
-//
-//    // Get and set current height
-//    var headerH = 62;
-//    var navigationH = $('#navigation').height();
-//    var contentH = $('.content').height();
-//
-//    // Set new height when contnet height is less then navigation
-//    if (contentH < navigationH) {
-//        $('#wrapper').css('min-height', navigationH + 'px');
-//    }
-//
-//    // Set new height when contnet height is less then navigation and navigation is less then window
-//    if (contentH < navigationH && navigationH < $(window).height()) {
-//        $('#wrapper').css('min-height', $(window).height() - headerH  + 'px');
-//    }
-//
-//    // Set new height when contnet is higher then navigation but less then window
-//    if (contentH > navigationH && contentH < $(window).height()) {
-//        $('#wrapper').css('min-height', $(window).height() - headerH + 'px');
-//    }
+
+    // Get and set current height
+    var headerH = 62;
+    var navigationH = $('#navigation').height();
+    var contentH = $('.content').height();
+
+    // Set new height when contnet height is less then navigation
+    if (contentH < navigationH) {
+        $('#wrapper').css('min-height', navigationH + 'px');
+    }
+
+    // Set new height when contnet height is less then navigation and navigation is less then window
+    if (contentH < navigationH && navigationH < $(window).height()) {
+        $('#wrapper').css('min-height', $(window).height() - headerH  + 'px');
+    }
+
+    // Set new height when contnet is higher then navigation but less then window
+    if (contentH > navigationH && contentH < $(window).height()) {
+        $('#wrapper').css('min-height', $(window).height() - headerH + 'px');
+    }
 }
 
 /**
