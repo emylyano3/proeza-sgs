912c791c14c7592a92b303b934b8b95e852c6e16
emylyano3 <emylyano3@gmail.com>
2015-09-22 09:49:20 -0300

    * Fix en el layout de la command thumbnails bar de las pantallas de articulo.Por el momento se anula la animacion de automatica de paneles para evitar que se corrompa el layout de las barras mencionadas

--- src/main/webapp/resources/app/js/proeza-sgs.js
+++ src/main/webapp/resources/app/js/proeza-sgs.js
@@ -159,34 +159,34 @@ function setBodySmall() {
 // Animate panel function
 $.fn['animatePanel'] = function() {
 
-    var element = $(this);
-    var effect = $(this).data('effect');
-    var delay = $(this).data('delay');
-    var child = $(this).data('child');
-
-    // Set default values for attrs
-    if(!effect) { effect = 'zoomIn'};
-    if(!delay) { delay = 0.06 } else { delay = delay / 10 };
-    if(!child) { child = '.row > div'} else {child = '.' + child};
-
-    //Set defaul values for start animation and delay
-    var startAnimation = 0;
-    var start = Math.abs(delay) + startAnimation;
-
-    // Get all visible element and set opactiy to 0
-    var panel = element.find(child);
-    panel.addClass('opacity-0');
-
-    // Get all elements and add effect class
-    panel = element.find(child);
-    panel.addClass('animated-panel').addClass(effect);
-
-    // Add delay for each child elements
-    panel.each(function (i, elm) {
-        start += delay;
-        var rounded = Math.round(start * 10) / 10;
-        $(elm).css('animation-delay', rounded + 's')
-        // Remove opacity 0 after finish
-        $(elm).removeClass('opacity-0');
-    });
+//    var element = $(this);
+//    var effect = $(this).data('effect');
+//    var delay = $(this).data('delay');
+//    var child = $(this).data('child');
+//
+//    // Set default values for attrs
+//    if(!effect) { effect = 'zoomIn'};
+//    if(!delay) { delay = 0.06 } else { delay = delay / 10 };
+//    if(!child) { child = '.row > div'} else {child = '.' + child};
+//
+//    //Set defaul values for start animation and delay
+//    var startAnimation = 0;
+//    var start = Math.abs(delay) + startAnimation;
+//
+//    // Get all visible element and set opactiy to 0
+//    var panel = element.find(child);
+//    panel.addClass('opacity-0');
+//
+//    // Get all elements and add effect class
+//    panel = element.find(child);
+//    panel.addClass('animated-panel').addClass(effect);
+//
+//    // Add delay for each child elements
+//    panel.each(function (i, elm) {
+//        start += delay;
+//        var rounded = Math.round(start * 10) / 10;
+//        $(elm).css('animation-delay', rounded + 's')
+//        // Remove opacity 0 after finish
+//        $(elm).removeClass('opacity-0');
+//    });
 }
