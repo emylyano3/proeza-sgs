2a2413c163d1cd2cbeb3b83188f4d85f732781c2
emylyano3 <emylyano3@gmail.com>
2015-09-23 16:09:24 -0300

    * Correccion en las consulta de los charts de articulo

--- src/main/resources/queries/native/articulo/charts/presenciaMarca.sql
+++ src/main/resources/queries/native/articulo/charts/presenciaMarca.sql
@@ -1,5 +1,5 @@
   SELECT m.nombre AS marca, sum(a.cantidad) AS stock
-    FROM sgs_proeza_db.art_articulo a
-         JOIN sgs_proeza_db.art_marca m ON m.id = a.fk_marca
+    FROM art_articulo a
+         JOIN art_marca m ON m.id = a.fk_marca
 GROUP BY m.nombre
 ORDER BY stock DESC
--- src/main/resources/queries/native/articulo/charts/rankingBestSellers.sql
+++ src/main/resources/queries/native/articulo/charts/rankingBestSellers.sql
@@ -1,6 +1,6 @@
   SELECT a.modelo AS modelo, sum(va.cantidad) AS cantidad
-    FROM sgs_proeza_db.art_venta_articulo va
-         JOIN sgs_proeza_db.art_venta v ON va.fk_venta = v.id
-         JOIN sgs_proeza_db.art_articulo a ON a.id = va.fk_articulo
+    FROM art_venta_articulo va
+         JOIN art_venta v ON va.fk_venta = v.id
+         JOIN art_articulo a ON a.id = va.fk_articulo
 GROUP BY modelo
 ORDER BY cantidad DESC
