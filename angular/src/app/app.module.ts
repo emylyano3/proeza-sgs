ace56dc485ea8365bcaa3c91e2dd39213972cab9
emylyano3 <emiliano.schiano@gmail.com>
2019-06-02 13:17:56 -0300

    ! maquetado general de la pantalla basica

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    MainMenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
