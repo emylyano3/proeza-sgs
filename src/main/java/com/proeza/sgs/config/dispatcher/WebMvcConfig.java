0198f30fec1aacbb32a1a73942fa8fb2fa2cbde4
emylyano3 <emiliano.schiano@gmail.com>
2019-06-06 17:09:53 -0300

    + Global CORS allow to angular host

--- src/main/java/com/proeza/sgs/config/dispatcher/WebMvcConfig.java
+++ src/main/java/com/proeza/sgs/config/dispatcher/WebMvcConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -39,6 +40,11 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+    
+    @Override
+    public void addCorsMappings (CorsRegistry registry) {
+    	registry.addMapping("*").allowedOrigins("http://localhost:4200");
     }
 
     @Bean
