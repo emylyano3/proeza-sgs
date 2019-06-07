f99133a743afc8ae1c0210cb4e93d8b2708ea08a
emylyano3 <emiliano.schiano@gmail.com>
2019-06-07 11:18:02 -0300

    + Hide menu (side bar)

--- angular/src/app/top-bar/top-bar.component.ts
+++ angular/src/app/top-bar/top-bar.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, OnInit, Inject, Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'app-top-bar',
-  templateUrl: './top-bar.component.html',
-  styleUrls: ['./top-bar.component.css']
+    selector: 'app-top-bar',
+    templateUrl: './top-bar.component.html',
+    styleUrls: ['./top-bar.component.css']
 })
 export class TopBarComponent implements OnInit {
 
-  constructor() { }
+    constructor(
+        @Inject(DOCUMENT) private document: Document,
+        private renderer: Renderer2,
+    ) { }
 
-  ngOnInit() {
-  }
+    showingMenu: boolean;
 
+    ngOnInit() {
+        this.showingMenu = true;
+    }
+
+    toggleMenu() {
+        if (this.showingMenu) {
+            this.renderer.addClass(this.document.body, 'hide-sidebar');
+        } else {
+            this.renderer.removeClass(this.document.body, 'hide-sidebar');
+        }
+        this.showingMenu = !this.showingMenu;
+    }
 }
