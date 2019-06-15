eee344c57a801e9feebee4988a64447c9a442f7f
emylyano3 <emiliano.schiano@gmail.com>
2019-06-15 13:29:28 -0300

    - ApplicationRestTest updated

--- src/test/java/proeza/test/integration/web/rest/ApplicationRestTest.java
+++ src/test/java/proeza/test/integration/web/rest/ApplicationRestTest.java
@@ -1,24 +1,22 @@
 package proeza.test.integration.web.rest;
 
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 import proeza.test.integration.WebMvcIntegrationTest;
 
 public class ApplicationRestTest extends WebMvcIntegrationTest {
 
-    @Test
-    public void priceHistory() throws Exception {
-        this.mockMvc
-        .perform(post("/rest/application/getMenu/MAIN"))
-        .andExpect(status().isOk())
-        .andExpect(jsonPath("$.items", hasSize(3)))
-        .andExpect(jsonPath("$.items[1].subitems", hasSize(4)))
-        .andExpect(jsonPath("$.name", is("MAIN")));
-    }
+	@Test
+	public void priceHistory() throws Exception {
+		this.mockMvc
+		.perform(get("/api/menu/MAIN/admin"))
+		.andExpect(status().isOk())
+		.andExpect(jsonPath("$.items", hasSize(3)))
+		.andExpect(jsonPath("$.items[1].subitems", hasSize(1)))
+		.andExpect(jsonPath("$.code", is("MAIN")));
+	}
 }
